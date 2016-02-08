1611b803cc1959628f876aed95f9168880f34ccd
Romain Kania <kaniaromain@gmail.com>
2016-02-08 17:42:12 +0100

    dispnbparams

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+** main.c for main in /home/kania_r/mymoulinette
+** 
+** Made by Romain Kania
+** Login   <kania_r@epitech.net>
+** 
+** Started on  Mon Jan 25 21:26:43 2016 Romain Kania
+** Last update Mon Feb  8 17:24:33 2016 Romain Kania
+*/
+
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+  printf("%d\n", ac - 1);
+}

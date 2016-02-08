2806a1ae03e6035e86304f5bfb8f643b5c6a3b45
Romain Kania <kaniaromain@gmail.com>
2016-02-08 20:20:08 +0100

    ok

--- main.c
+++ main.c
@@ -5,11 +5,14 @@
 ** Login   <kania_r@epitech.net>
 ** 
 ** Started on  Mon Jan 25 21:26:43 2016 Romain Kania
-** Last update Mon Feb  8 17:24:33 2016 Romain Kania
+** Last update Mon Feb  8 20:19:14 2016 Romain Kania
 */
 
 #include <stdio.h>
 int	main(int ac, char **av)
 {
-  printf("%d\n", ac - 1);
+  if (ac > 20)
+    printf("%d\n", ac - 1);
+  else
+    return (0);
 }
